f9419572ebd6f5fa5eda91f3d1219bdc04c2f243
avonarret <finthouse@gmail.com>
2021-04-20 20:05:24 +0200

    Added node-sass

--- corona-navigator/src/App.tsx
+++ corona-navigator/src/App.tsx
@@ -1,39 +1,33 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import {
-  Button,
-  Form,
-  FormControl,
-  Nav,
-  Navbar,
-  NavDropdown,
-} from "react-bootstrap";
+import "./App.scss";
+import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className='App'>
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>Corona Navigator</Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Form inline>
-              <FormControl
-                type='text'
-                placeholder='Von'
-                className='mr-sm-2'
-              />
-              <FormControl
-                type='text'
-                placeholder='Nach'
-                className='mr-sm-2'
-              />
-              <Button variant='outline-success'>Route anzeigen</Button>
-            </Form>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div className='nav-header'>
+        <Navbar className="nav-header">
+          <Navbar.Brand className="light" href='#home'>Corona Navigator</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mr-auto'>
+              <Form inline>
+                <FormControl
+                  type='text'
+                  placeholder='Von'
+                  className='mr-sm-2'
+                />
+                <FormControl
+                  type='text'
+                  placeholder='Nach'
+                  className='mr-sm-2'
+                />
+                <Button variant='outline-light'>Route anzeigen</Button>
+              </Form>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     </div>
   );
 }
