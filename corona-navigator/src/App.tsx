0a83d68c0e468279ad0f402a13cc924e3aacee0d
avonarret <finthouse@gmail.com>
2021-04-20 19:46:16 +0200

    Added Bootstrap and simple Navbar

--- corona-navigator/src/App.tsx
+++ corona-navigator/src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Button,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home'>Corona Navigator</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Form inline>
+              <FormControl
+                type='text'
+                placeholder='Von'
+                className='mr-sm-2'
+              />
+              <FormControl
+                type='text'
+                placeholder='Nach'
+                className='mr-sm-2'
+              />
+              <Button variant='outline-success'>Route anzeigen</Button>
+            </Form>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
