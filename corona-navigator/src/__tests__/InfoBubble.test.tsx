6efc1768c7f6ac3d5a7cd167221442c1f3354474
avonarret <finthouse@gmail.com>
2021-05-17 16:02:29 +0200

    Removed transformIgnorePatterns due to Jest Mocks

--- corona-navigator/src/__tests__/InfoBubble.test.tsx
+++ corona-navigator/src/__tests__/InfoBubble.test.tsx
@@ -1,5 +1,3 @@
-jest.mock("react-icons/all", () => "Icon"); // https://medium.com/xebia/de-mystifying-jest-snapshot-test-mocks-8e7183d109ea
-jest.mock("react-icons", () => "Icon");
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
 import { cleanup, render, screen } from "@testing-library/react";
@@ -13,66 +11,73 @@ describe("infobubble component renders correctly", () => {
         cleanup();
     });
     
-    // in test file.
-    beforeAll(() => {});
-
     test("render infobubble", () => {
-        // given
-        const currentDate = new Date();
-        render(
-          <IntlProvider locale='de-DE'>
-            <InfoBubble lat={"47.390434"} lng={"8.0457015"} data={
-                {
-                  show:       true,
-                  lat:        "47.390434",
-                  lng:        "8.0457015",
-                  name:       "Aarau",
-                  zip:        5001,
-                  incidence:  132.092,
-                  date:       currentDate.toString(),
-                }
-            } />
-          </IntlProvider>
-        );
-      
-        // when
-        const infobubbleElement = screen.getByTestId("municipality-zip-name");
-        const infobubbleIncidenceElement = screen.getByTestId("municipality-incidence");
-        const infobubbleDateElement = screen.getByTestId("municipality-date");
-      
-        // then
-        expect(infobubbleElement).toBeInTheDocument();
-        expect(infobubbleElement).toHaveTextContent("5001 Aarau");
-        expect(infobubbleIncidenceElement).toBeInTheDocument();
-        expect(infobubbleIncidenceElement).toHaveTextContent("132.1");
-        expect(infobubbleDateElement).toBeInTheDocument();
-        expect(infobubbleDateElement).toHaveTextContent(moment(currentDate).format("DD.MM.YYYY"));
-      });
+      // given
+      const currentDate = new Date();
+      render(
+        <IntlProvider locale='de-DE'>
+          <InfoBubble
+            lat={"47.390434"}
+            lng={"8.0457015"}
+            data={{
+              show: true,
+              lat: "47.390434",
+              lng: "8.0457015",
+              name: "Aarau",
+              zip: 5001,
+              incidence: 132.092,
+              date: currentDate.toString(),
+            }}
+          />
+        </IntlProvider>
+      );
+
+      // when
+      const infobubbleElement = screen.getByTestId("municipality-zip-name");
+      const infobubbleIncidenceElement = screen.getByTestId(
+        "municipality-incidence"
+      );
+      const infobubbleDateElement = screen.getByTestId("municipality-date");
+
+      // then
+      expect(infobubbleElement).toBeInTheDocument();
+      expect(infobubbleElement).toHaveTextContent("5001 Aarau");
+      expect(infobubbleIncidenceElement).toBeInTheDocument();
+      expect(infobubbleIncidenceElement).toHaveTextContent("132.1");
+      expect(infobubbleDateElement).toBeInTheDocument();
+      expect(infobubbleDateElement).toHaveTextContent(
+        moment(currentDate).format("DD.MM.YYYY")
+      );
+    });
 
 
       test("render info missing incidence", () => {
         // given
         render(
           <IntlProvider locale='de-DE'>
-            <InfoBubble lat={"47.390434"} lng={"8.0457015"} data={
-                {
-                  show:       true,
-                  lat:        "47.390434",
-                  lng:        "8.0457015",
-                  name:       "Aarau",
-                  zip:        5001,
-                  incidence:  undefined,
-                  date:       undefined,
-                }
-            } />
+            <InfoBubble
+              lat={"47.390434"}
+              lng={"8.0457015"}
+              data={{
+                show: true,
+                lat: "47.390434",
+                lng: "8.0457015",
+                name: "Aarau",
+                zip: 5001,
+                incidence: undefined,
+                date: undefined,
+              }}
+            />
           </IntlProvider>
         );
-      
+
         // when
         const infobubbleElement = screen.getByTestId("municipality-zip-name");
-        const infobubbleIncidenceElement = screen.getByTestId("municipality-incidence");
+        const infobubbleIncidenceElement = screen.getByTestId(
+          "municipality-incidence"
+        );
         const infobubbleDateElement = screen.getByTestId("municipality-date");
-      
+
         // then
         expect(infobubbleElement).toBeInTheDocument();
         expect(infobubbleElement).toHaveTextContent("5001 Aarau");
